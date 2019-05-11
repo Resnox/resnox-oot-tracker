4850fcf44a1fa6fa7f355740d116f4e37993fb35
Resnox <bastien.rimbert@laposte.net>
2019-05-11 09:45:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ const server = express()
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-  socket.on('itemchanges', (data) => socket.broadcast.emit('itemchangec', data));
-  socket.on('statechanges', (data) => socket.broadcast.emit('statechangec', data));
+  socket.on('item-change', (data) => socket.broadcast.emit('item-change-back', data));
+  socket.on('hint-change', (data) => socket.broadcast.emit('hint-change-back', data));
+  socket.on('zone-change', (data) => socket.broadcast.emit('zone-change-back', data));
 });
